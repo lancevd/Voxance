0a6281324db9d6761ea53674fb1addb9b0a646ad
lancevd <olamosob@gmail.com>
2025-03-27 04:50:17 +0100

    login

--- app/context/authContext.js
+++ app/context/authContext.js
@@ -20,21 +20,20 @@ export const AuthProvider = ({ children }) => {
       const response = await axiosInstance.get(`/auth/check`);
       if (response.status === 401) {
         router.push("/login");
-        setLoading(false)
+        setLoading(false);
       }
       console.log(response.data.message);
       setUser(response.data.user);
-        setLoading(false);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const register =  async (data) => {
+  const register = async (data) => {
     setLoading(true);
     try {
       const response = await axiosInstance.post(`/auth/register`, data);
-      console.log(response.data.message);
       setUser(response.data.user);
       toast.success(response.data.message);
       setLoading(false);
@@ -42,12 +41,36 @@ export const AuthProvider = ({ children }) => {
     } catch (error) {
       console.error(error);
       toast.error(error.response.data.message);
-      setLoading(false) 
+      setLoading(false);
     }
-  }
+  };
+
+  const login = async (data) => {
+    setLoading(true);
+    console.log(data);
+    try {
+      const response = await axiosInstance.post("/auth/login", data);
+      if (!response.data)
+        toast.error(
+          "An error ocurred. Try again later or reach out to our support"
+        );
+      setUser(response.data.user);
+      setLoading(false);
+      toast.success(response.data.message);
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error)
+      toast.error(error.response.data.message);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ user, setUser, checkUserLoggedIn, register }}>
+    <AuthContext.Provider
+      value={{ user, setUser, checkUserLoggedIn, register, login, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,40 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "../context/authContext";
+import toast from "react-hot-toast";
+import Spinner from "../components/Spinner";
 
 const page = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { login, loading } = useAuth();
+
+  const validateForm = () => {
+    if (!formData.email.trim() || !formData.password.trim()) {
+      toast.error("Please fill all fields.");
+      return false;
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      toast.error("Invalid email address.");
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const valid = validateForm();
+    if (valid === true) {
+     login(formData);
+    }
+  };
+
   return (
     <main className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -10,8 +43,11 @@ const page = () => {
             <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Welcome Back!
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              
+            <form
+              onSubmit={handleSubmit}
+              method="POST"
+              className="space-y-4 md:space-y-6"
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -23,6 +59,10 @@ const page = () => {
                   type="email"
                   name="email"
                   id="email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required
@@ -39,6 +79,10 @@ const page = () => {
                   type="password"
                   name="password"
                   id="password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required
@@ -49,7 +93,7 @@ const page = () => {
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Create an account
+                {loading ? <Spinner /> : "Login"}
               </button>
               <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">
                 Don't have an account? &nbsp;
