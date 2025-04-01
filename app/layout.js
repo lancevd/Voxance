c9b5648893c2c320683ed3748b90aa433c43d5e7
lancevd <olamosob@gmail.com>
2025-04-01 18:03:49 +0100

    suppressHydrationWarning  html tag

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="bg-gray-50 dark:bg-gray-900">
         <AuthProvider>
           <NavBarWrapper />
