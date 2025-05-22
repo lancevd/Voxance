f088a0a11ca75774368ed1251a74c1a878421c39
lancevd <olamosob@gmail.com>
2025-05-22 10:22:55 +0100

    changed base url to vercel

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5100/api" : "https://voxance-backend.onrender.com/api",
-    withCredentials: true
-})
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5100/api"
+      : "https://voxance-backend.vercel.app/",
+  withCredentials: true,
+});
